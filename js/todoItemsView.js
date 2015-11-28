6183e776ab986a350a51375b249b672ec7e5576b
Hector Rios <hrios@sugarcrm.com>
2015-11-28 22:23:59 +0100

    Add textbox to new todos and enhance UX by adding when "enter" is hit and escaping the description of the todo to prevent hacking

--- js/todoItemsView.js
+++ js/todoItemsView.js
@@ -8,13 +8,24 @@ var TodoItemsView = Backbone.View.extend({
 
     id: 'todoItems',
 
+    events: {
+        'click #add': 'onClickAdd',
+        'keypress #newTodoItem': 'onKeyPress'
+    },
+
     initialize: function(options) {
         if (!(options && options.model)) {
             throw new Error('model is not specified');
         }
+
+        this.model.on('add', this.onAddTodoItem, this);
     },
 
     render: function () {
+
+        this.$el.append('<input type="text" id="newTodoItem" autofocus >' );
+
+        this.$el.append('<button id="add">Add</button>');
 
         this.model.each(function (todoItem) {
             var view = new TodoItemView({ model: todoItem});
@@ -22,5 +33,27 @@ var TodoItemsView = Backbone.View.extend({
         }, this);
 
         return this;
+    },
+
+    onClickAdd: function () {
+        var $textBox = this.$('#newTodoItem');
+        var newDescription = $textBox.val();
+        if (newDescription && newDescription.length > 2) {
+            var todoItem = new TodoItem({description: newDescription});
+            this.model.add(todoItem);
+            $textBox.val("");
+        }
+    },
+
+    onAddTodoItem: function (newTodoItem) {
+        var view = new TodoItemView({model: newTodoItem});
+
+        this.$el.append(view.render().$el);
+    },
+
+    onKeyPress: function (e) {
+        if (e.keyCode === 13) {
+            this.onClickAdd();
+        }
     }
 });
